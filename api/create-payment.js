20b3b9f39d8ccded000847a1af11ad049e0403bb
Just-4U <deepmeanings1978@gmail.com>
2025-12-11 05:41:35 +0200

    Update create-payment.js

--- api/create-payment.js
+++ api/create-payment.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
-        price_amount: 14.99,
+        price_amount: 0.2,
         price_currency: "USD",
 
         // 🔥 Correct code for USDT TRON (Invoice endpoint)
@@ -39,3 +39,4 @@ export default async function handler(req, res) {
     res.status(500).json({ error: "Server error" });
   }
 }
+
